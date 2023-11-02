b06baf27d9be62027259ef2e45edc1203933131e
AdrianMvpDev <adrianimpor1@gmail.com>
2023-11-02 04:56:59 -0500

    fixed edit

--- src/components/common/EditModal.js
+++ src/components/common/EditModal.js
@@ -1,44 +1,44 @@
-import { useState, useCallback, lazy, Suspense } from 'react';
-import { fetchUserData, createUserData } from '../../services/api';
+import { useState, useEffect, useCallback, lazy, Suspense } from 'react';
+import { fetchUserData, updateUserData } from '../../services/api';
 import ModalButtons from './ModalButtons';
 const LazyInputField = lazy(() => import('./InputField'));
 const LazySelectField = lazy(() => import('./SelectField'));
 
-export default function CreateModal({ isOpen, onClose, setUserData }) {
-  const [newUserData, setNewUserData] = useState({
-    title: '',
-    firstName: '',
-    lastName: '',
-    picture: '',
-  });
+export default function EditModal({ isOpen, onClose, item, setUserData }) {
+  const [editedItem, setEditedItem] = useState(item || {});
   const [isSaving, setIsSaving] = useState(false);
+
+  useEffect(() => {
+    if (item) {
+      setEditedItem(item);
+    }
+  }, [item]);
 
   const handleFieldChange = useCallback(
     (e) => {
       const { name, value } = e.target;
-      setNewUserData({
-        ...newUserData,
+      setEditedItem({
+        ...editedItem,
         [name]: value,
       });
     },
-    [newUserData]
+    [editedItem]
   );
 
   const handleSave = useCallback(async () => {
     setIsSaving(true);
     try {
-      await createUserData(newUserData);
+      await updateUserData(editedItem.id, editedItem);
+      const refreshedData = await fetchUserData();
       setIsSaving(false);
       onClose();
-
-      const refreshedData = await fetchUserData();
       setUserData(refreshedData);
     } catch (error) {
-      console.error('Error al crear un nuevo usuario:', error);
+      console.error('Error al actualizar los datos:', error);
       setIsSaving(false);
       onClose();
     }
-  }, [newUserData, onClose, setUserData]);
+  }, [editedItem, onClose, setUserData]);
 
   return (
     <div
@@ -49,14 +49,14 @@ export default function CreateModal({ isOpen, onClose, setUserData }) {
       <div className="relative w-full max-w-2xl max-h-full m-auto shadow">
         <div className="relative bg-white rounded-lg shadow border-[#1a6e6a]">
           <div className="flex items-start justify-between p-4 border-b rounded-t">
-            <h2 className="text-lg font-semibold text-gray-800">Crear Usuario</h2>
+            <h2 className="text-lg font-semibold text-gray-800">Editar Usuario</h2>
           </div>
           <form className="p-4 border-b rounded-t space-y-3">
             <Suspense fallback={<div>Cargando...</div>}>
-              <LazySelectField label="Título" name="title" value={newUserData.title} onChange={handleFieldChange} />
-              <LazyInputField label="Nombres" name="firstName" value={newUserData.firstName} onChange={handleFieldChange} />
-              <LazyInputField label="Apellidos" name="lastName" value={newUserData.lastName} onChange={handleFieldChange} />
-              <LazyInputField label="Foto" name="picture" value={newUserData.picture} onChange={handleFieldChange} />
+              <LazySelectField label="Título" name="title" value={editedItem.title} onChange={handleFieldChange} />
+              <LazyInputField label="Nombres" name="firstName" value={editedItem.firstName} onChange={handleFieldChange} />
+              <LazyInputField label="Apellidos" name="lastName" value={editedItem.lastName} onChange={handleFieldChange} />
+              <LazyInputField label="Foto" name="picture" value={editedItem.picture} onChange={handleFieldChange} />
             </Suspense>
           </form>
           <ModalButtons onCancel={onClose} onSave={handleSave} isSaving={isSaving} textButton={'Guardar'} textButton2={'Guardando...'} />
